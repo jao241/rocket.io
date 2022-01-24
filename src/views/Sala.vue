6c373b1f83f52e32cec1e09ba28d57eb9b0d10f4
joao vitor <vorque45@gmail.com>
2022-01-24 17:25:09 -0300

    nova página em andamento

---
+++ src/views/Sala.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="sala">
+        <header>
+            <logo/>
+            <options/>
+        </header>
+        <h2>Faça a sua pergunta</h2>
+        <textarea name="" id=""></textarea>
+        <button>Enviar</button>
+    </div>
+</template>
+
+<script>
+import Logo from '../components/Logo.vue'
+import Options from '../components/Options.vue'
+
+export default {
+  components: { Logo, Options },
+    name: 'Sala'
+}
+</script>
+
+<style scoped>
+
+.sala header{
+    display: flex;
+}
+
+</style>

d286c054477e3340df5062c594fdd27c5127c552
joao vitor <vorque45@gmail.com>
2022-01-24 17:24:44 -0300

    nova rota adicionada

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import CriarSala from '../views/CriarSala.vue'
+import Sala from '../views/Sala.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/criar',
     name: 'CriarSala',
     component: CriarSala
+  },
+  {
+    path: '/sala',
+    name: 'Sala',
+    component: Sala
   }
 ]
 

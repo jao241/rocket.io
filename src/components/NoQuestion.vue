29fd64047ac5d80197d409f74434d5cff519ddcd
joao vitor <vorque45@gmail.com>
2022-02-08 16:19:46 -0300

    criei um componente para quando não existirem perguntas

---
+++ src/components/NoQuestion.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="sem-perguntas">
+        <img src="@/assets/baloes-conversa.svg" alt="baloes de conversa">
+        <h3>Nenhuma pergunta por aqui...</h3>
+        <p>Faça sua primeira pergunta e envie o código dessa sala para outras pessoas!</p>
+    </section>
+</template>
+
+<style scoped>
+
+.sala .sem-perguntas{
+    width: 17vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;    
+}
+
+.sala .sem-perguntas h3{
+    font-family: 'Poppins', sans-serif;
+    font-size: 18px;
+    color: #0D114F;
+}
+
+.sala .sem-perguntas p{
+    font-family: 'Open Sans', sans-serif;
+    font-size: 14px;
+    color: #4D5E77;
+}
+</style>

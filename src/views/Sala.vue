3f39f51e2b432f3a8163c290baefe3fd15e23c98
joao vitor <vorque45@gmail.com>
2022-02-08 16:18:58 -0300

    tornei parte do html em componentes

--- src/views/Sala.vue
+++ src/views/Sala.vue
@@ -8,49 +8,31 @@
             <h2>Faça a sua pergunta</h2>
             <!-- <textarea name="" id="" placeholder="O que você quer perguntar?"></textarea> -->
             <div class="novaPergunta">
-                <input type="text" name="pergunta" id="pergunta" placeholder="O que você quer perguntar?">
+                <input type="text" name="pergunta" id="pergunta" placeholder="O que você quer perguntar?" v-model="newQuestion">
                 <div class="texto">
                     <img src="@/assets/cadeado.svg" alt="cadeado"><p>Esta pergunta é anônima</p>
-                    <button>Enviar</button>
+                    <button @click="addQuestion">Enviar</button>
                 </div>
             </div>
         </section>
-        <section class="sem-perguntas" v-show="!hasQuestions">
-            <img src="@/assets/baloes-conversa.svg" alt="baloes de conversa">
-            <h3>Nenhuma pergunta por aqui...</h3>
-            <p>Faça sua primeira pergunta e envie o código dessa sala para outras pessoas!</p>
-        </section>
-        <section class="com-perguntas">
-            <div class="pergunta-bloco" v-for="question in questions" :key="question.id">
-                <div class="pergunta">
-                    <img src="@/assets/anonimo.svg" alt="icone anonimo">
-                    <p>{{question.question}}</p>
-                </div>
-                <div class="opcoes">
-                    <div class="checar">
-                        <img src="@/assets/check-icone.svg" alt="icone de checado">
-                        <p>Marcar como lida</p>
-                    </div>
-                    <div class="remover">
-                        <img src="@/assets/trash-icone.svg" alt="icone de lixeira">
-                        <p>Excluir</p>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <no-question v-show="!hasQuestions"/>
+        <question v-for="question in questions" :key="question.id" :question="question"/>
     </div>
 </template>
 
 <script>
 import Logo from '../components/Logo.vue'
 import Options from '../components/Options.vue'
+import Question from '../components/Question.vue'
+import NoQuestion from '../components/NoQuestion.vue'
 
 export default {
-  components: { Logo, Options },
+  components: { Logo, Options, Question, NoQuestion },
     name: 'Sala',
     data(){
         return{
-            questions: [{
+            questions: [
+            {
                 id: 1,
                 question: 'Qual a diferença entre const, var e let no Javascript?'
             },
@@ -62,12 +44,26 @@ export default {
                 id: 3,
                 question: 'O que é Javascript?'
             }
-            ]
+            ],
+            newQuestion: '',
+            count: 4
         }
     },
     computed:{
         hasQuestions(){
             return this.questions.length > 0 ? true : false;
+        }
+    },
+    methods: {
+        addQuestion(){
+            const objQuestion = {
+                id: this.count,
+                question: this.newQuestion
+            }
+
+            this.questions.unshift(objQuestion);
+            this.count++;
+            this.newQuestion = '';
         }
     }
 }
@@ -85,7 +81,7 @@ export default {
 
 .sala section{
     width: 80%;
-    margin: 50px auto 0;
+    margin: 20px auto 0 auto;
 }
 
 .sala section div{
@@ -133,67 +129,4 @@ export default {
     color: #A5B0C1;
 }
 
-.sala .sem-perguntas{
-    width: 17vw;
-    display: flex;
-    flex-direction: column;
-    align-items: center;    
-}
-
-.sala .sem-perguntas h3{
-    font-family: 'Poppins', sans-serif;
-    font-size: 18px;
-    color: #0D114F;
-}
-
-.sala .sem-perguntas p{
-    font-family: 'Open Sans', sans-serif;
-    font-size: 14px;
-    color: #4D5E77;
-}
-
-.pergunta-bloco{
-    background-color: #E0ECFF;
-    border-radius: 0 3px 3px;
-    justify-content: initial;
-    flex-direction: column;
-    margin-bottom: 10px;
-}
-
-.pergunta{
-    padding-top: 20px;
-}
-
-.pergunta img{
-    width: 52px;
-    height: 52px;
-    padding: 0 10px;
-}
-
-.pergunta p{
-    font-family: 'Open sans', sans-serif;
-    font-size: 16px;
-}
-
-.opcoes{
-    margin-left: 80%;
-}
-
-.opcoes img{
-    width: 24px;
-    height: 24px;
-    padding-right: 5px;
-}
-
-.opcoes p{
-    padding-right: 10px;
-    font-family: 'Open sans', sans-serif;
-    color: #4D5E77;
-}
-
-.checar,
-.remover{
-    align-items: center;
-    cursor: pointer;
-}
 </style>
